ca7991c425912a685c8e9094bbeb8c148b438d59
Bhupender Gaur <150256375+bh-gaur@users.noreply.github.com>
2025-09-07 18:12:06 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1757248926235

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ce0c432a._.js",
+  "static/chunks/src_09c0de9b._.js"
+],
+    source: "dynamic"
+});
